8aa83d2e3866b0f6889422927b6b288f44b6ba87
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-04-22 17:24:20 +0300

    add tabs

---
+++ js7-8tabs/js/JavaScript1.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    $(".tabs-menu a").click(function (event) {
+        event.preventDefault();
+        console.log('event', event)
+        $(this).parent().addClass("current");
+        console.log('this')
+        $(this).parent().siblings().removeClass("current");
+        console.log('this', this)
+        var tab = $(this).attr("href");
+        console.log('tab', tab)
+        $(".tab-content").not(tab).css("display", "none");      
+        $(tab).fadeIn();
+       
+    });
+});
+
